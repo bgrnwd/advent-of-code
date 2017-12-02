02b508d942f9950fae770f23746f275943af8a8d
Brian Greenwood <bgwood18@gmail.com>
2017-12-02 14:54:09 -0500

    initial commit

---
+++ Day1.py
@@ -0,0 +1,29 @@
+# Day 1
+def Day1():
+    # Variable declarations
+    array = []
+    total = 0
+
+    # Open input file
+    with open("./inputs/input1.txt", "r") as file:
+        for line in file:
+            for char in line:
+                # for char in file, append to array as an int
+                array.append(int(char))
+
+    # Add 1st element to end of array for circularity
+    array.append(array[0])
+
+    # Loop through array and check next index for match
+    for num in range(0, len(array) - 1):
+        if array[num] == array[num+1]:
+            # Add to total
+            total += array[num]
+    # Print answer
+    print total
+
+def main():
+    Day1()
+
+if __name__ == "__main__":
+    main()          
